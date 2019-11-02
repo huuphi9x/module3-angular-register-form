6ced9ba73d398a13bfd32135e31ad34d9303124d
admin <huuphi9x@gmail.com>
2019-11-02 20:46:17 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'module3-angular-reactive-form';
+  title = 'angular-form-register';
 }
